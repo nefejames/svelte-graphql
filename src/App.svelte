1d4b73271b411222103a496c087312d9d03c527c
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-06-26 07:31:21 -0700

    svelte graphql integration

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Todos from "./Todos.svelte";
+  import ApolloClient from "apollo-boost";
+  import { setClient } from "svelte-apollo";
+  const client = new ApolloClient({
+    uri: "http://todos-graphql.herokuapp.com/",
+  });
+
+  setClient(client);
+</script>
+
+<main>
+  <h1>Svelte GraphQl Todo App</h1>
+  <Todos />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 2em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Todos.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { getClient, query } from "svelte-apollo";
+  import { gql } from "apollo-boost";
+  const GET_TODOS = gql`
+    {
+      getTodos {
+        id
+        text
+        done
+      }
+    }
+  `;
+
+  const client = getClient();
+
+  const todos = query(client, { query: GET_TODOS });
+</script>
+
+<h1>Todos</h1>
